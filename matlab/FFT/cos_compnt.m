2b164d25a4c13bbf8fc75791fc2cb1e67898f69e
Katkov Maxim <mkatkov355@gmail.com>
2023-11-08 22:06:17 +0300

    Delete folder

--- matlab/FFT/cos_compnt.m
+++
@@ -1,5 +0,0 @@
-function c = cos_compnt(x, n, m, N, Pi)
-    
-    c = x * ( cos( (2*Pi*n*m) / N ) ); 
-
-    
--- matlab/FFT/sin_compnt.m
+++
@@ -1,3 +0,0 @@
-function s = sin_compnt(x, n, m, N, Pi)
-
-    s = x * ( sin( (2*Pi*n*m) / N ) );
