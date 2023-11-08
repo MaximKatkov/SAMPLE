7cb6fff78ae6a203751099753b2a087946d67431
Katkov Maxim <mkatkov355@gmail.com>
2023-11-08 12:38:06 +0300

    FFT2.m
    В векторном виде

--- matlab/FFT/FFT2.m
+++ matlab/FFT/FFT2.m
@@ -2,26 +2,20 @@ function X = FFT2(x)
  
     N = length(x);
     N_half = N/2;
-    Pi = 3.141592653589793;
-    XRe = zeros(1,N_half);
-    XIm = zeros(1,N_half);
-    X = zeros(1,N_half);
+    m = (0:1:N_half-1)';
 
-    for m = 0:N_half-1
+    a1=0;a2=0;b1=0;b2=0;
 
-        for n = 0:N_half-1
+    for n = 0:N_half-1
 
-            a1 = cos_compnt( x(2*n+1), n, m, (N/2), Pi );
-            a2 = cos_compnt( x(2*n + 2), (1+2*n), m, N, Pi );
-                
-            b1 = sin_compnt( x(2*n+1), n, m, (N/2), Pi );
-            b2 = sin_compnt( x(2*n + 2), (1+2*n), m, N, Pi );
-            
-            XRe(m+1) = XRe(m+1) + a1 + a2;
-            XIm(m+1) = XIm(m+1) - b1 - b2;  
+            a1 = a1 + (cos_compnt( x(2*n+1), n, m, (N/2) ));
+            a2 = a2 + (cos_compnt( x(2*n + 2), (1+2*n), m, N ));
 
-        end
+            b1 = b1 + (sin_compnt( x(2*n+1), n, m, (N/2) ));
+            b2 = b2 + (sin_compnt( x(2*n + 2), (1+2*n), m, N ));
 
-        X(m+1) = sqrt((XRe(m+1))^2 + (XIm(m+1))^2);
+    end
 
-    end   
+    X = ((( + a1 + a2)).^2 + (( - b1 - b2)).^2).^(0.5);
+
+end
